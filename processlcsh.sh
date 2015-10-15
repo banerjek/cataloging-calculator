224ecdc209874fbed60f610e4d3b17d8eef0e090
Kyle Banerjee <banerjek@ohsu.edu>
2015-10-15 16:21:22 -0700

    script to process LCSH LOD files

---
+++ processlcsh.sh
@@ -0,0 +1,8 @@
+## processes LOD files from lcsh. Still need to join lines in output file
+fgrep 'subjects/sh' *.nt |fgrep prefLabel | sed 's/^.*subjects\/sh/sh/' |sed 's/>.*<.*> "/\t/' | sed 's/"@.*$//' | awk '{print $2,$1}{FS="\t"}{OFS="\t"}' | sed 's/"//g' | sed 's/@/(at)/' | sort -u > goodheadings
+
+echo 'var lcsh="' > lcsh.js
+cat goodheadings | tr "\n" "@" >> lcsh.js 
+echo '";' >> lcsh.js
+cat lcsh.js |tr -d "\n"  > junk
+mv junk lcsh.js
