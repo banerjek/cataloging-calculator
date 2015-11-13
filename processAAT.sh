a066cfaba8ba18719e238dde3d0f068ab3043702
Kyle Banerjee <banerjek@ohsu.edu>
2015-11-13 15:21:38 -0800

    fix AAT processing script and update data file

--- processAAT.sh
+++ processAAT.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-fgrep 'skos-xl#literalForm>' AATOut_2Terms.nt  | fgrep '-en>' | sed 's/^<[^>]*term\///' |sed 's/-en> <[^>]*>/\t/' | sed 's/"@en.*$//' |sed 's/ *"/	/' | lang=eng_ENG sort > terms
+fgrep 'prefLabelGVP>' AATOut_2Terms.nt  | sed 's/<[^>]*aat\///g' | sed 's/> <[^>]*>//' | sed 's/^<[^>]*term\///' |sed 's/[^0-9 ]//g' |awk '{print $2,$1}' |sort |sed 's/ /\t/' > preflabel
 
+fgrep 'skos-xl#literalForm>' AATOut_2Terms.nt  | fgrep '-en>' | sed 's/^<[^>]*term\///' |sed 's/-en> <[^>]*>/\t/' | sed 's/"@en.*$//' | sed '/[<>]/ d' | sed 's/ *"//' | lang=en_EN sort -k1,1 > terms
 
-fgrep 'skos-xl#literalForm>' AATOut_2Terms.nt  | fgrep '-en>' | sed 's/^<[^>]*term\///' |sed 's/-en> <[^>]*>/\t/' | sed 's/"@en.*$//' |sed 's/ *"//' | lang=en_EN sort -k1,1 > terms
+join -t "	" preflabel terms > joined 
 
-join -t "	" preflabel terms |awk '{FS="\t"}{OFS="\t"}{print $2,$3}' |sort -k2 |tr "\n" '@' |sed '/[<>]/ d' > aat.js 
+cat joined | awk '{FS="\t"}{OFS="\t"}{print $2,$3}' |sort -k2 |tr "\n" '@' > aat.js 
